3d7e37b4be28cd9a10af4ebe1048e29e050bee5f
Sergio Cano <sergiocano.dev@gmail.com>
2024-09-01 16:02:50 +0200

    feat: Add database integration using PrismaService

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
+  constructor(private prismaService: PrismaService) {}
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    this.prismaService.product.create({ data: createProductDto });
   }
 
   findAll() {
