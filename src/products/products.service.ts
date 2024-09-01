18451176faf7da6b2a77f8162af03c5b52bfcaf0
Sergio Cano <sergiocano.dev@gmail.com>
2024-09-01 16:19:39 +0200

    feat: Add error handling to findOne service in products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -15,8 +15,14 @@ export class ProductsService {
     return this.prismaService.product.findMany();
   }
 
-  findOne(id: number) {
-    return this.prismaService.product.findUnique({ where: { id: id } });
+  async findOne(id: number) {
+    const productFound = await this.prismaService.product.findUnique({
+      where: { id: id },
+    });
+    if (!productFound) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return productFound;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
