0003fe3836fa25415516852cffc15e32e3190769
Sergio Cano <sergiocano.dev@gmail.com>
2024-09-01 16:01:57 +0200

    chore: Update CreateProductDto to exclude certain fields

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,5 @@
-export class CreateProductDto {}
+// export class CreateProductDto {}
+
+import { Product } from '@prisma/client';
+
+export type CreateProductDto = Omit<Product, 'id' | 'createdAt' | 'updatedAt'>;
