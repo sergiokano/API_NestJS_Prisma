1a5ac053df58163ae02d41ed67f62cd226f2cbbc
Sergio Cano <sergiocano.dev@gmail.com>
2024-09-02 17:50:20 +0200

    feat: Add 'Products' tag to product controller for Swagger API documentation

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -10,8 +10,9 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
