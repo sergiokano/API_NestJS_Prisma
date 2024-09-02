50d473136cdb4b3d452c634b738b294c353d36d5
Sergio Cano <sergiocano.dev@gmail.com>
2024-09-02 17:34:35 +0200

    refactor: Update API documentation title and description

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('API documentation')
+    .setDescription('This is the API documentation for the products API developed by Sergio Cano')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('Products')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
