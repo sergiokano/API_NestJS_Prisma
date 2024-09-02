9472421f433a9782df5cb6da97607528d08d83ce
Sergio Cano <sergiocano.dev@gmail.com>
2024-09-02 17:34:58 +0200

    feat: Add to swagger API documentation -> error handling to product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,33 +1,75 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a product' })
+  @ApiResponse({
+    status: 201,
+    description: 'The product has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad request.' })
+  @ApiResponse({
+    status: 409,
+    description: 'Conflict. The product already exists.',
+  })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all products' })
+  @ApiResponse({
+    status: 200,
+    description: 'The products have been successfully retrieved.',
+  })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a product by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'The product has been successfully retrieved.',
+  })
+  @ApiResponse({ status: 404, description: 'Product not found.' })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a product' })
+  @ApiResponse({
+    status: 200,
+    description: 'The product has been successfully updated.',
+  })
+  @ApiResponse({ status: 404, description: 'Product not found.' })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a product' })
+  @ApiResponse({
+    status: 200,
+    description: 'The product has been successfully deleted.',
+  })
+  @ApiResponse({ status: 404, description: 'Product not found.' })
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }
